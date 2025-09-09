3dd64acc904af8faa09b1d894e0fd5449c5e16f0
Federico Yaroslavsky <v-federico.yaroslavsky@stagwellglobal.com>
2025-09-09 01:26:57 -0300

    Add tests folder

---
+++ tests/test_users.py
@@ -0,0 +1,19 @@
+def test_create_user():
+    # TODO
+    pass
+
+def test_get_user_by_id():
+    # TODO
+    pass
+
+def test_get_all_users():
+    # TODO
+    pass
+
+def test_update_user():
+    # TODO
+    pass
+
+def test_delete_user():
+    # TODO
+    pass
